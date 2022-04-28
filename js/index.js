05650e05b38ea514afbafc441f1d5a56556faf05
DarthVauban <Dante2770@gmail.com>
2022-04-28 17:43:14 +0300

    Главная страница

---
+++ js/index.js
@@ -0,0 +1,60 @@
+const burgerMenu = document.querySelector('.menu-icon')
+const mobileMenu = document.querySelector('.mobile-menu')
+const overlayMobile = document.querySelector('.mobile-menu-overlay')
+const body = document.querySelector('body')
+
+burgerMenu.addEventListener('click',() => {
+    mobileMenu.classList.add('mobile-menu-active')
+    overlayMobile.style.display = 'block'
+    body.style.overflow = 'hidden'
+})
+
+overlayMobile.addEventListener('click', () => {
+    if (mobileMenu.classList.contains ('mobile-menu-active')) {
+        mobileMenu.classList.remove('mobile-menu-active')
+    }
+    overlayMobile.style.display = 'none'
+    body.style.overflow = 'visible'
+})
+
+$( function() {
+    $('.slider-cards').slick({
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+})
+
