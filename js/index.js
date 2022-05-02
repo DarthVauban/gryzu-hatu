e9a76937cd57f1ff5c3728d78177257bbcd8cc0b
DarthVauban <Dante2770@gmail.com>
2022-05-02 12:41:30 +0300

    Главная страница

--- js/index.js
+++ js/index.js
@@ -21,7 +21,7 @@ $( function() {
     $('.slider-cards').slick({
         dots: false,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 5000,
         infinite: true,
         arrows: true,
         speed: 300,
@@ -56,5 +56,46 @@ $( function() {
           // instead of a settings object
         ]
       });
+})
+
+$( function() {
+  $('.review-cards').slick({
+      dots: false,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      infinite: true,
+      arrows: true,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
 })
 
