b701e2b1e60a0b28387be7f280586700922d6977
DarthVauban <Dante2770@gmail.com>
2022-05-06 15:01:55 +0300

    Страница твоара

---
+++ js/goods.js
@@ -0,0 +1,38 @@
+const burgerMenu = document.querySelector('.menu-icon')
+const mobileMenu = document.querySelector('.mobile-menu')
+const overlayMobile = document.querySelector('.mobile-menu-overlay')
+const body = document.querySelector('body')
+
+burgerMenu.addEventListener('click',() => {
+    mobileMenu.classList.add('mobile-menu-active')
+    overlayMobile.style.display = 'block'
+    body.style.overflow = 'hidden'
+})
+
+overlayMobile.addEventListener('click', () => {
+    if (mobileMenu.classList.contains ('mobile-menu-active')) {
+        mobileMenu.classList.remove('mobile-menu-active')
+    }
+    overlayMobile.style.display = 'none'
+    body.style.overflow = 'visible'
+})
+
+
+$(function () {
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+      });
+      $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+      });
+})
+
