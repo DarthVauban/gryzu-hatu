e56b96fd42855b323d10544acf9dc8bb8c66b0fa
DarthVauban <Dante2770@gmail.com>
2022-05-10 15:50:14 +0300

    Страница товара

--- js/goods.js
+++ js/goods.js
@@ -70,9 +70,6 @@ $(function () {
               slidesToScroll: 1
             }
           }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
         ]
       });
 })
